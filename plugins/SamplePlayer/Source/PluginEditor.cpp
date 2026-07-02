34ba26ac1d3449076ce1eee15c98276b57c96a99
sasadukic <117151977+sasadukic@users.noreply.github.com>
2026-07-02 13:16:56 +0200

    Include windows.h to define SetEnvironmentVariableW on Windows

--- plugins/SamplePlayer/Source/PluginEditor.cpp
+++ plugins/SamplePlayer/Source/PluginEditor.cpp
@@ -2,6 +2,11 @@
 #include "PluginEditor.h"
 #include "BinaryData.h"
 
+#if JUCE_WINDOWS
+ #define WIN32_LEAN_AND_MEAN
+ #include <windows.h>
+#endif
+
 #include <algorithm>
 #include <cmath>
 #include <cstring>
